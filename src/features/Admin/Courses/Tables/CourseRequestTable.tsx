6c6404df9c03e3ea9a7ba63987f8b270465f2669
Ranjith <ranjithp2294@gmail.com>
2024-03-19 10:23:47 +0530

    design has slightly modified for course section in admin dash bord

--- src/features/Admin/Courses/Tables/CourseRequestTable.tsx
+++ src/features/Admin/Courses/Tables/CourseRequestTable.tsx
@@ -27,124 +27,107 @@ const CourseRequestTable = () => {
 
   useEffect(() => {
     setTableData(coursesInRequest);
-    console.log("table data ",tableData)
+    console.log("table data ", tableData);
   }, [coursesInRequest, tableData]);
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-        <Thead>
-          <tr>
-            <Th>Sl No</Th>
-            <Th>Course</Th>
-            <Th>Instructor</Th>
-            <Th>Status</Th>
-            <Th>Action</Th>
-          </tr>
-        </Thead>
+    <>
+      {tableData && tableData.length ? (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+            <Thead>
+              <tr>
+                <Th>Sl No</Th>
+                <Th>Course</Th>
+                <Th>Instructor</Th>
+                <Th>Status</Th>
+                <Th>Action</Th>
+              </tr>
+            </Thead>
 
-      
-        <tbody>
-          {tableData.map((item: ICourse, index: number) => (
-            <TableBodyTr
-              lastIndex={tableData.length !== index + 1}
-              index={index}
+            <tbody>
+              {tableData.map((item: ICourse, index: number) => (
+                <TableBodyTr
+                  lastIndex={tableData.length !== index + 1}
+                  index={index}
+                >
+                  <Td>{index + 1}</Td>
+                  <td
+                    className="px-6 py-4"
+                    onClick={() => {
+                      navigate("/admin/dash_bord/course_preview", {
+                        state: { courseData: item },
+                      });
+                    }}
+                  >
+                    {item.courseName.toUpperCase()}
+                  </td>
+                  <Td>{item.instructor}</Td>
+                  <Td>{item.status}</Td>
+                  <Td>
+                    <div className="flex justify-start gap-3 ">
+                      <button
+                        className="dark:bg-cyan-500 py-1 px-4 rounded-full"
+                        onClick={() => {
+                          approveOrRejectCourse({
+                            courseId: item._id,
+                            action: "approved",
+                          });
+                        }}
+                      >
+                        Approve
+                      </button>
+                      <button
+                        className="dark:bg-red-700 py-1 px-4 rounded-full"
+                        onClick={() => {
+                          approveOrRejectCourse({
+                            courseId: item._id,
+                            action: "rejected",
+                          });
+                        }}
+                      >
+                        Reject
+                      </button>
+                    </div>
+                  </Td>
+                </TableBodyTr>
+              ))}
+            </tbody>
+          </table>
+          <div className=" dark:bg-c_color-colorSeven p-3 flex justify-end gap-1">
+            <h4 className="me-2">4 of 5</h4>
+            <input
+              type="text"
+              className="w-[30px] h-[25px] text-black p-0 text-center rounded-md"
+            />
+            <select
+              name=""
+              id=""
+              className="w-[120px] h-[25px] text-black p-0 ps-2 text-center rounded-md"
             >
-              <Td>{index + 1}</Td>
-              <td
-                className="px-6 py-4"
-                onClick={() => {
-                  navigate("/admin/dash_bord/course_preview", {
-                    state: { courseData: item },
-                  });
-                }}
-              >
-                {item.courseName.toUpperCase()}
-              </td>
-              <Td>{item.instructor}</Td>
-              <Td>{item.status}</Td>
-              <Td>
-                <div className="flex justify-start gap-3 ">
-                  <button
-                    className="dark:bg-cyan-500 py-1 px-4 rounded-full"
-                    onClick={() => {
-                      approveOrRejectCourse({
-                        courseId: item._id,
-                        action: "approved",
-                      });
-                    }}
-                  >
-                    Approve
-                  </button>
-                  <button
-                    className="dark:bg-red-700 py-1 px-4 rounded-full"
-                    onClick={() => {
-                      approveOrRejectCourse({
-                        courseId: item._id,
-                        action: "rejected",
-                      });
-                    }}
-                  >
-                    Reject
-                  </button>
-                </div>
-              </Td>
-            </TableBodyTr>
-
-            // <tr
-            //   className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            //   key={index}
-            // >
-            //   <td className="px-6 py-4 cursor-pointer">{index + 1}</td>
-            //   <td
-            //     className="px-6 py-4 cursor-pointer"
-            //     onClick={() => {
-            //       navigate("/course_preview", { state: { courseData: item } });
-            //     }}
-            //   >
-            //     {item.courseName}
-            //   </td>
-            //   <td className="px-6 py-4 cursor-pointer">{item.instructor}</td>
-            //   <td className="px-6 py-4 cursor-pointer">{item.status}</td>
-            //   <td className="px-6 py-4 cursor-pointer">
-            //     <div className="flex justify-start gap-3 ">
-            //       <button className="bg-gray-50 dark:bg-gray-700 py-1 px-2 rounded-md">
-            //         Approve
-            //       </button>
-            //       <button className="bg-gray-50 dark:bg-gray-700 py-1 px-2 rounded-md">
-            //         Reject
-            //       </button>
-            //     </div>
-            //   </td>
-            // </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className=" dark:bg-c_color-colorSeven p-3 flex justify-end gap-1">
-        <h4 className="me-2">4 of 5</h4>
-        <input
-          type="text"
-          className="w-[30px] h-[25px] text-black p-0 text-center rounded-md"
-        />
-        <select
-          name=""
-          id=""
-          className="w-[120px] h-[25px] text-black p-0 ps-2 text-center rounded-md"
-        >
-          <option value="">5 pages</option>
-          <option value="">10 pages</option>
-          <option value="">25 pages</option>
-          <option value="">50 pages</option>
-          <option value="">100 pages</option>
-        </select>
-        <div className="flex gap-2 mx-2 items-center">
-          <FaBackward />
-          <IoCaretBack />
-          <IoCaretForwardOutline />
-          <FaForward />
+              <option value="">5 pages</option>
+              <option value="">10 pages</option>
+              <option value="">25 pages</option>
+              <option value="">50 pages</option>
+              <option value="">100 pages</option>
+            </select>
+            <div className="flex gap-2 mx-2 items-center">
+              <FaBackward />
+              <IoCaretBack />
+              <IoCaretForwardOutline />
+              <FaForward />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          {" "}
+          <h1 className="text-4xl font-semibold italic">
+            No courses  requests have been found 
+          </h1>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/features/Admin/Courses/Tables/CourseTable.tsx
+++ src/features/Admin/Courses/Tables/CourseTable.tsx
@@ -27,77 +27,88 @@ const CourseTable = () => {
   }, [coursesData]);
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
-      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
-        <Thead>
-          <tr>
-            <Th>Sl No</Th>
-            <Th>Course</Th>
-            <Th>Instructor</Th>
-            <Th>No Purchase</Th>
-            <Th>Status</Th>
-            <Th>Freez</Th>
-          </tr>
-        </Thead>
+    <>
+      {tableData && tableData.length ? (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg ">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 ">
+            <Thead>
+              <tr>
+                <Th>Sl No</Th>
+                <Th>Course</Th>
+                <Th>Instructor</Th>
+                <Th>No Purchase</Th>
+                <Th>Status</Th>
+                <Th>Freez</Th>
+              </tr>
+            </Thead>
 
-        <tbody>
-          {tableData.map((item: ICourse, index: number) => (
-            <TableBodyTr
-              lastIndex={tableData.length !== index + 1}
-              index={index}
+            <tbody>
+              {tableData.map((item: ICourse, index: number) => (
+                <TableBodyTr
+                  lastIndex={tableData.length !== index + 1}
+                  index={index}
+                >
+                  <Td>{index + 1}</Td>
+                  <td
+                    className="px-6 py-4"
+                    onClick={() => {
+                      navigate(`user_details/${item._id}`);
+                    }}
+                  >
+                    {item.courseName.toUpperCase()}
+                  </td>
+                  <Td>{item.instructor}</Td>
+                  <Td>{item.noOfPurchase}</Td>
+                  <Td>{item.status}</Td>
+                  <Td>
+                    {item.status === "approved" ? (
+                      <button className="border rounded-full px-5 hover:text-red-600 hover:scale-105">
+                        Freez
+                      </button>
+                    ) : (
+                      <button className="border rounded-full px-5 hover:text-yellow-300 hover:scale-105">
+                        Un Freez
+                      </button>
+                    )}
+                  </Td>
+                </TableBodyTr>
+              ))}
+            </tbody>
+          </table>
+          <div className=" dark:bg-c_color-colorSeven p-3 flex justify-end gap-1">
+            <h4 className="me-2">4 of 5</h4>
+            <input
+              type="text"
+              className="w-[30px] h-[25px] text-black p-0 text-center rounded-md"
+            />
+            <select
+              name=""
+              id=""
+              className="w-[120px] h-[25px] text-black p-0 ps-2 text-center rounded-md"
             >
-              <Td>{index + 1}</Td>
-              <td
-                className="px-6 py-4"
-                onClick={() => {
-                  navigate(`user_details/${item._id}`);
-                }}
-              >
-                {item.courseName.toUpperCase()}
-              </td>
-              <Td>{item.instructor}</Td>
-              <Td>{item.noOfPurchase}</Td>
-              <Td>{item.status}</Td>
-              <Td>
-                {item.status === "approved" ? (
-                  <button className="border rounded-full px-5 hover:text-red-600 hover:scale-105">
-                    Freez
-                  </button>
-                ) : (
-                  <button className="border rounded-full px-5 hover:text-yellow-300 hover:scale-105">
-                    Un Freez
-                  </button>
-                )}
-              </Td>
-            </TableBodyTr>
-          ))}
-        </tbody>
-      </table>
-      <div className=" dark:bg-c_color-colorSeven p-3 flex justify-end gap-1">
-        <h4 className="me-2">4 of 5</h4>
-        <input
-          type="text"
-          className="w-[30px] h-[25px] text-black p-0 text-center rounded-md"
-        />
-        <select
-          name=""
-          id=""
-          className="w-[120px] h-[25px] text-black p-0 ps-2 text-center rounded-md"
-        >
-          <option value="">5 pages</option>
-          <option value="">10 pages</option>
-          <option value="">25 pages</option>
-          <option value="">50 pages</option>
-          <option value="">100 pages</option>
-        </select>
-        <div className="flex gap-2 mx-2 items-center">
-          <FaBackward />
-          <IoCaretBack />
-          <IoCaretForwardOutline />
-          <FaForward />
+              <option value="">5 pages</option>
+              <option value="">10 pages</option>
+              <option value="">25 pages</option>
+              <option value="">50 pages</option>
+              <option value="">100 pages</option>
+            </select>
+            <div className="flex gap-2 mx-2 items-center">
+              <FaBackward />
+              <IoCaretBack />
+              <IoCaretForwardOutline />
+              <FaForward />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          {" "}
+          <h1 className="text-4xl font-semibold italic">
+            No courses have been created yet
+          </h1>
+        </div>
+      )}
+    </>
   );
 };
 
