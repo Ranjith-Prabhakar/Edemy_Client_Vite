3d6a43e2e8734c4b5a96a547655eaa73f2b5c5fe
Ranjith <ranjithp2294@gmail.com>
2024-04-17 13:37:02 +0530

    clean up for src/components/card/courseCard.tsx has been done

--- src/components/Card/CourseCard.tsx
+++ src/components/Card/CourseCard.tsx
@@ -14,7 +14,6 @@ import toast from "react-hot-toast";
 import useGetUser from "../../hooks/useGetUser";
 
 type Props = {
-  // courseCategory: ICourse[];
   courseCategory: ICourse;
 };
 
@@ -147,64 +146,4 @@ const CourseCard = ({ courseCategory }: Props): ReactNode => {
 
 export default CourseCard;
 
-{
-  /* <div className="flex justify-start gap-3  w-full overflow-x-scroll">
-  {courseCategory.map((item, index) => (
-    <div
-      key={index}
-      className="flex flex-col w-[300px] rounded-2xl overflow-hidden dark:bg-c_color-colorSeven shadow-lg pb-4 space-y-2"
-    >
-      <img
-        src={
-          // getThumbnailImagesPreSignedUrl({ thumbnail: item.thumbnail }) ||
-          imgUrl || web_development
-        }
-        alt=""
-        className="w-full h-[200px]"
-        onClick={() => {
-          navigate("/course_single_page", {
-            state: { courseData: item },
-          });
-        }}
-      />
-      <h4
-        className="text-[#fffff] mt-2 text-xl ps-4 cursor-pointer"
-        onClick={() => {
-          navigate("/course_single_page", {
-            state: { courseData: item },
-          });
-        }}
-      >
-        {item.courseName}
-      </h4>
-      <div className="space-y-2">
-        <div className="flex justify-between px-4">
-          <div className="flex ">
-            <FaStar color={"#fffff"} />
-            <FaStar color={"#fffff"} />
-            <FaStar color={"#fffff"} />
-            <FaStar color={"#fffff"} />
-            <FaStarHalf color={"#fffff"} />
-          </div>
-          <div className="flex items-center ">
-            <FaRupeeSign />
-            <h3>{item.price}</h3>
-          </div>
-        </div>
-        <div className="flex px-4 justify-between">
-          <div className="flex gap-1 items-center">
-            <MdFormatListBulletedAdd size={20} />
-            <h4>{item.modules.map((module) => module.videos.length)}</h4>
-          </div>
-          <div className="">
-            <button className="bg-white text-gray-950 py-1 px-2 rounded-sm font-semibold">
-              Enroll Now
-            </button>
-          </div>
-        </div>
-        <div className=""></div>
-      </div>
-    </div>
-  ))}
-</div>; */
-}
+
