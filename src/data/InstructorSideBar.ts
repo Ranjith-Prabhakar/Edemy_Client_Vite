b7ff84c652a9bed1674eda89ecbd875f9031cc43
Ranjith <ranjithp2294@gmail.com>
2024-04-20 15:46:39 +0530

    dashbord item has been removed form the side bar for instructor

--- src/data/InstructorSideBar.ts
+++ src/data/InstructorSideBar.ts
@@ -1,4 +1,4 @@
-import { AiOutlineLayout } from "react-icons/ai";
+// import { AiOutlineLayout } from "react-icons/ai";
 import { FaPhotoVideo } from "react-icons/fa";
 import { IoIosNotifications } from "react-icons/io";
 import { IoIosChatbubbles } from "react-icons/io";
@@ -6,7 +6,7 @@ import { MdOutlineEventNote } from "react-icons/md";
 import { FaYoutube } from "react-icons/fa";
 
 export const instructorSideBar = [
-  { name: "Dashboard", icon: AiOutlineLayout },
+  // { name: "Dashboard", icon: AiOutlineLayout },
   { name: "Add Courses", icon: FaPhotoVideo },
   { name: "My Tutorials", icon: FaYoutube },
   { name: "Notifications", icon: IoIosNotifications },
