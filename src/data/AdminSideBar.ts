eeada6f07162850b633060f8d32fbbac14b997c9
Ranjith <ranjithp2294@gmail.com>
2024-04-20 15:57:26 +0530

    unused items have been removed form the side bar for admin

--- src/data/AdminSideBar.ts
+++ src/data/AdminSideBar.ts
@@ -3,11 +3,11 @@ import { FaUsers } from "react-icons/fa";
 import { GiTeacher } from "react-icons/gi";
 import { TbCategoryPlus } from "react-icons/tb";
 import { FaPhotoVideo } from "react-icons/fa";
-import { FaMoneyCheckDollar } from "react-icons/fa6";
-import { MdAccountBalance } from "react-icons/md";
-import { IoIosNotifications } from "react-icons/io";
-import { IoIosChatbubbles } from "react-icons/io";
-import { MdOutlineEventNote } from "react-icons/md";
+// import { FaMoneyCheckDollar } from "react-icons/fa6";
+// import { MdAccountBalance } from "react-icons/md";
+// import { IoIosNotifications } from "react-icons/io";
+// import { IoIosChatbubbles } from "react-icons/io";
+// import { MdOutlineEventNote } from "react-icons/md";
 
 export const adminDashBord = [
   { name: "Dashboard", icon: AiOutlineLayout },
@@ -15,9 +15,9 @@ export const adminDashBord = [
   { name: "Instructors", icon: GiTeacher },
   { name: "Categories", icon: TbCategoryPlus },
   { name: "Courses", icon: FaPhotoVideo },
-  { name: "Payments", icon: FaMoneyCheckDollar },
-  { name: "Accounts", icon: MdAccountBalance },
-  { name: "Notifications", icon: IoIosNotifications },
-  { name: "Chat", icon: IoIosChatbubbles },
-  { name: "Event", icon: MdOutlineEventNote },
+  // { name: "Payments", icon: FaMoneyCheckDollar },
+  // { name: "Accounts", icon: MdAccountBalance },
+  // { name: "Notifications", icon: IoIosNotifications },
+  // { name: "Chat", icon: IoIosChatbubbles },
+  // { name: "Event", icon: MdOutlineEventNote },
 ];
