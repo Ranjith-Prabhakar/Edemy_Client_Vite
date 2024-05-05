134b06a85af0aa0059c5a4bca01dfa365dbcf18c
Ranjith <ranjithp2294@gmail.com>
2024-05-05 10:59:48 +0530

    deployment error resolved by adding props for instructor table component

--- src/Routes/InstructorRoute.tsx
+++ src/Routes/InstructorRoute.tsx
@@ -19,14 +19,16 @@ const InstructorRoute = () => {
   return (
     <div>
       {user.role === "instructor" && (
-      
         <ContainerLayout>
           <Header />
           <div className="h-[87vh] flex justify-start mt-1 gap-2 pb-3">
             <SideBar sideBarData={instructorSideBar} />
             <div className="flex flex-col gap-2 h-full overflow-scroll scroll-m-1 dark:bg-c_color-colorOne shadow-md ring-gray-400   rounded-md w-full relative ">
               <Routes>
-                <Route path="dashboard" element={<Table />} />
+                <Route
+                  path="dashboard"
+                  element={<Table sideBarData={instructorSideBar} />}
+                />
                 <Route path="addcourses" element={<Courses />} />
                 <Route path="mytutorials" element={<MyTutorials />} />
                 <Route path="payment_success" element={<PaymentSucess />} />
